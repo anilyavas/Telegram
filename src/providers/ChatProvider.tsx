a951d08f9f6d9a78a075df797da6dcd4fae19147
anilyavas <anil.yavas99@gmail.com>
2024-05-08 19:55:19 +0300

    redirecting and full authentication

--- src/providers/ChatProvider.tsx
+++ src/providers/ChatProvider.tsx
@@ -2,16 +2,19 @@ import { PropsWithChildren, useEffect, useState } from 'react';
 import { ActivityIndicator } from 'react-native';
 import { StreamChat } from 'stream-chat';
 import { Chat, OverlayProvider } from 'stream-chat-expo';
+import { useAuth } from './AuthProvider';
 
 const client = StreamChat.getInstance(process.env.EXPO_PUBLIC_STREAM_API_KEY);
 
 export default function ChatProvider({ children }: PropsWithChildren) {
   const [isReady, setIsReady] = useState(false);
+  const { user } = useAuth();
+
   useEffect(() => {
     const connect = async () => {
       await client.connectUser(
         {
-          id: 'jlahey',
+          id: user.id,
           name: 'Jim Lahey',
           image: 'https://i.imgur.com/fR9Jz14.png',
         },
